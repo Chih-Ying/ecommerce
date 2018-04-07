8bf29a007da6438a13fbb7854a69b10c749dca39
ChihYing <ChangChihYing@cl2-wifi-10-215-46-98.wireless.pitt.edu>
2018-04-07 17:03:32 -0400

    H3

--- index.php
+++ index.php
@@ -2,31 +2,31 @@
 <html>
 	<head>
 		<title>My Online Shop</title>
-		
-	<link rel="stylesheet" href="styles/style.css" media="all" />	
+
+	<link rel="stylesheet" href="styles/style.css" media="all" />
 	</head>
-	
-	
+
+
 <body>
 
-<h1>Beauty Diaries</h1>
+<h3>Beauty Diaries</h3>
 
 	<!--Main Container starts here-->
 	<div class="main_wrapper">
-	
+
 		<!--Header starts here-->
 		<div class="header_wrapper">
-		
+
 			<img id="logo" src="image/logo.jpg" style="width:250px; height:150px;" />
 			<img id="banner" src="image/ad.jpg" style="width:250px; height:150px;" />
-		
+
 		</div>
 		<!--Header ends here-->
-		
+
 		<!--Navigation Bar starts-->
 		<div class="menubar">
 			<ul id="nav_bar">
-			
+
 				<li class="nav_button"><a href="#">Home</a></li>
 				<li class="nav_button"><a href="#">All Products</a></li>
 				<li class="nav_button"><a href="#">My Account</a></li>
@@ -35,26 +35,26 @@
 				<li class="nav_button"><a href="#">Contact Us</a></li>
 
 			</ul>
-			
+
 			<div id="form">
 				<form method="get" action="results.php" enctype="multipart/form-data">
 					<input type="text" name="user_query" placeholder="Search a Product"/>
 					<input type="submit" name="search" value="Search" />
 				</form>
 			</div>
-			
+
 		</div>
 		<!--Navigation Bar ends-->
-		
+
 		<!--Content wrapper starts-->
 		<div class="content_wrapper">
-		
+
 			<div id="sidebar">
-			
+
 				<div id="sidebar_title">Categories</div>
-				
+
 				<ul id="cats">
-				
+
 					<li><a href="#">Face</a></li>
 					<li><a href="#">Cheek</a></li>
 					<li><a href="#">Eye</a></li>
@@ -62,13 +62,13 @@
 					<li><a href="#">Brushes&Applications</a></li>
 					<li><a href="#">Accessories</a></li>
 					<li><a href="#">Nail</a></li>
-				
+
 				</ul>
-				
+
 				<div id="sidebar_title">Brands</div>
-				
+
 				<ul id="cats">
-				
+
 					<li><a href="#">NARS</a></li>
 					<li><a href="#">YSL</a></li>
 					<li><a href="#">DIOR</a></li>
@@ -77,18 +77,18 @@
 					<li><a href="#">TOO FACED</a></li>
 					<li><a href="#">TOM FORD</a></li>
 					<li><a href="#">MARC JACOBS BEAUTY</a></li>
-				
+
 				</ul>
-				
+
 			</div>
 
 			<div id="content_area">This is content area</div>
 		</div>
 		<!--Content wrapper ends-->
-		
+
 		<div id="footer">This is the footer</div>
 	</div>
 	<!--Main Container ends here-->
-		
+
 </body>
 </html>
